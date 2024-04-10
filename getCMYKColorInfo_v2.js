18126775809a77d879f0c9ec9b8b24facf9fd6fe
Ben Hoath <140858998+thewavyblue@users.noreply.github.com>
2024-04-10 17:45:00 +0100

    v2.0 beta
    Rather than an 'alert' popup dialog to get the colour values, I want to add a text field to the document with the values in. I'm half-way through making that possible with this version.

---
+++ getCMYKColorInfo_v2.js
@@ -0,0 +1,22 @@
+if (app.documents.length && app.selection.length >= 1 && app.selection[0].hasOwnProperty('fillColor'))
+{
+    swatch = app.selection[0].fillColor;
+    swatchArr = [swatch.colorValue];
+    cVal = "C" + Math.round(swatchArr[0][0]);
+    mVal = "M" + Math.round(swatchArr[0][1]);
+    yVal = "Y" + Math.round(swatchArr[0][2]);
+    kVal = "K" + Math.round(swatchArr[0][3]);
+
+    if (swatch instanceof Color && swatch.space == ColorSpace.CMYK) {
+        output = "CMYK color: " + cVal + " " + mVal + " " + yVal + " " + kVal;
+        alert (output);
+    } else {
+        alert ("Not a CMYK colour. Make sure you have checked this colour is using the CMYK space.");
+    }
+
+    
+    var myPage = app.documents.item(0); // This currently only targets page 1, but needs to target the current active page
+    var myTextFrame = myPage.textFrames.add();
+    myTextFrame.contents = output;
+    myTextFrame.geometricBounds = [0, 0, 10, 100]; // LocationX, LocationY, HeightY, WidthX (dims are in mm)
+}
